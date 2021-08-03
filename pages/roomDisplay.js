8974980dca83048ff6e5e4cfd421b0da5347cf29
klaus michaelson <wtfmichaelson@gmail.com>
2021-08-03 06:36:05 -0400

    don't commit this

--- pages/roomDisplay.js
+++ pages/roomDisplay.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { Image, InputNumber, Checkbox, Dropdown, Menu, Input, Button, Space, DatePicker, Collapse, Slider } from "antd";
-import { CalendarOutlined, DownOutlined, UsergroupAddOutlined } from '@ant-design/icons';
-import styles from '../styles/style.module.css';
-
+import {
+  Image,
+  InputNumber,
+  Checkbox,
+  Dropdown,
+  Menu,
+  Input,
+  Button,
+  Space,
+  DatePicker,
+  Collapse,
+  Slider,
+} from "antd";
+import {
+  CalendarOutlined,
+  DownOutlined,
+  UsergroupAddOutlined,
+} from "@ant-design/icons";
+import styles from "../styles/style.module.css";
 
 const { RangePicker } = DatePicker;
 const { Panel } = Collapse;
 const { Search } = Input;
 const { SubMenu } = Menu;
-
 
 export class GuestBookInfo extends React.Component {
   state = {
@@ -16,87 +30,107 @@ export class GuestBookInfo extends React.Component {
     endDate: null,
     guests: {
       adults: 0,
-      childs: 0
-    }
-  }
+      childs: 0,
+    },
+  };
   render() {
     const dropdown = (
       <div className={styles.gBookDropdownBox}>
-        <div className='row'>
-          <span className='pd'>
+        <div className="row">
+          <span className="pd">
             <CalendarOutlined />
           </span>
           <RangePicker placeholder={["Check-in Date", "Check-out Date"]} />
         </div>
         <br />
         <div className="row">
-          <span className='pd'>
+          <span className="pd">
             <UsergroupAddOutlined />
           </span>
           <div>
-            <h3 >Guests</h3>
-            <div className='row'>
-              <span className='pd'>
-                Adults: <InputNumber min={1} max={10} defaultValue={0} bordered={false} />
+            <h3>Guests</h3>
+            <div className="row">
+              <span className="pd">
+                Adults:{" "}
+                <InputNumber
+                  min={1}
+                  max={10}
+                  defaultValue={0}
+                  bordered={false}
+                />
               </span>
-              <span className='pd'>
-                Children: <InputNumber min={1} max={10} defaultValue={0} bordered={false} />
+              <span className="pd">
+                Children:{" "}
+                <InputNumber
+                  min={1}
+                  max={10}
+                  defaultValue={0}
+                  bordered={false}
+                />
               </span>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
     return (
-      <div className={'mrg-ver ' + styles.gBookInfo}>
+      <div className={"mrg-ver " + styles.gBookInfo}>
         <div>
           <Dropdown overlay={dropdown} trigger={["click"]}>
             <div>
-              <Button size={'large'} onClick={ev => ev.preventDefault()}>
-                <CalendarOutlined className='' />
+              <Button size={"large"} onClick={(ev) => ev.preventDefault()}>
+                <CalendarOutlined className="" />
                 Add Booking Info
               </Button>
             </div>
           </Dropdown>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export class SearchBox extends React.Component {
-  state = {
-
-  }
+  state = {};
   render() {
     const filter = (
       <Menu>
         <Menu.ItemGroup title="Price">
-          <Slider range defaultValue={[100, 2500]}
-            min={10} max={10000} step={100}></Slider>
+          <Slider
+            range
+            defaultValue={[100, 2500]}
+            min={10}
+            max={10000}
+            step={100}
+          ></Slider>
         </Menu.ItemGroup>
         <Menu.ItemGroup title="Amenities">
           <Checkbox>Wifi</Checkbox>
           <Checkbox>Shower</Checkbox>
         </Menu.ItemGroup>
       </Menu>
-    )
+    );
     return (
-      <div className='row mrg-ver flexcntnt-cntr pd'>
+      <div className="row mrg-ver flexcntnt-cntr pd">
         <span>
           <Search placeholder="Search Rooms" allowClear enterButton />
         </span>
-        <span >
-          <Dropdown overlay={filter}
+        <span>
+          <Dropdown
+            overlay={filter}
             placement="bottomRight"
-            trigger={['click']}>
-            <Button className='ant-dropdown-link' onClick={e => e.preventDefault()}>
+            trigger={["click"]}
+          >
+            <Button
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
               Filter <DownOutlined />
             </Button>
           </Dropdown>
         </span>
       </div>
-    )
+    );
   }
 }
 
@@ -146,60 +180,51 @@ export default class RoomDisplay extends React.Component {
         description: "Room description in detail goes here",
         price: 1231,
         discount: 12,
-      }
+      },
     ],
   };
 
   render() {
     return (
-      <>
+      <div>
         <SearchBox />
         <GuestBookInfo />
         <div className="row flexcntnt-cntr flex-wrap">
-          {
-            this.state.list.map((room) => (
-              <div className={"w-md test " + styles.roomDisp}>
-                <div className={styles.imgCont}>
-                  <Image.PreviewGroup >
-                    {
-                      room.imgs.map((src) => {
-                        return <Image src={src} />
-                      })
-                    }
-                  </Image.PreviewGroup>
-                  <small className={styles.avs}>
-                    {room.availableMsg}
-                  </small>
+          {this.state.list.map((room) => (
+            <div key={room.id} className={"w-md test " + styles.roomDisp}>
+              <div className={styles.imgCont}>
+                <Image.PreviewGroup>
+                  {room.imgs.map((src) => {
+                    return <Image src={src} />;
+                  })}
+                </Image.PreviewGroup>
+                <small className={styles.avs}>{room.availableMsg}</small>
+              </div>
+              <div className={styles.descr}>
+                <h2>{room.type}</h2>
+                <div>
+                  {room.beds} beds | &nbsp;
+                  {room.sleeps} sleeps
                 </div>
-                <div className={styles.descr}>
-                  <h2>{room.type}</h2>
-                  <div>
-                    {room.beds} beds | &nbsp;
-                    {room.sleeps} sleeps
-                  </div>
-                  <div className={styles.detail}>
-                    <Collapse
-                      bordered={false}
-                      defaultActiveKey={[]} ghost>
-                      <Panel header="Room Details" key="1">
-                        {room.description}
-                      </Panel>
-                    </Collapse>
-                  </div>
-                  <hr />
-                  <div>
-                    ETB {room.price} / night
-                  </div>
-                  <div className="row">
-                    <Button style={{ flex: 1 }} type="primary">BOOK NOW</Button>
-                  </div>
+                <div className={styles.detail}>
+                  <Collapse bordered={false} defaultActiveKey={[]} ghost>
+                    <Panel header="Room Details" key="1">
+                      {room.description}
+                    </Panel>
+                  </Collapse>
+                </div>
+                <hr />
+                <div>ETB {room.price} / night</div>
+                <div className="row">
+                  <Button style={{ flex: 1 }} type="primary">
+                    BOOK NOW
+                  </Button>
                 </div>
               </div>
-            )
-            )
-          }
+            </div>
+          ))}
         </div>
-      </>
+      </div>
     );
   }
 }
